1289f97d2df1addb81355ec4c382d47ba15b9b6c
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-17 18:30:50 -0600

    settings updates

--- travelogue_django/settings.py
+++ travelogue_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import django_heroku
 import os
+import dj-database-url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -109,7 +110,8 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-
+db_from_env = dj_database_url.config()
+DATABASES[‘default’].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
