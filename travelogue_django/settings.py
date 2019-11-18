172faf75a28b5f2d93341739e4c19aba618a524b
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-17 18:32:28 -0600

    settings change

--- travelogue_django/settings.py
+++ travelogue_django/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import django_heroku
 import os
-import dj-database-url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
